ba59f4033b74e9823e68c623dadd4f78cb09f98c
agontar <agontar@yandex.ru>
2023-01-24 17:14:43 +0300

    test & debug calc_priorities function

--- Lesson8/Seminar8/Seminar8.py
+++ Lesson8/Seminar8/Seminar8.py
@@ -1,44 +1,69 @@
-def calc(a, b, ch):
-    if ch == '+':
+def calc(a, b, op): # элементарные операции
+    if op == '+':
         return a + b
-    elif ch == '-':
+    elif op == '-':
         return a - b
-    elif ch == '/':
+    elif op == '/':
         return a / b
-    elif ch == '*':
+    elif op == '*':
         return a * b
 
-def calc_no_priorities(m : []):
-     result = int(m[0])
+def calc_no_priorities(m : []): # вычисление строки без приоритетов
+     result = float(m[0])
      for i in range(1, len(m) - 1, 2):
-         result = calc(result, int(m[i + 1]), m[i])         
+         result = calc(result, float(m[i + 1]), m[i])         
      return result
  
-def calc_priorities(m : []):
+
+def calc_priority(m : []): # вычисление и удаление одной приоритетной операции
      for i in range(1, len(m) - 1, 2):
        if m[i] == '*' or m[i] == '/':
-        result = calc(int(m[i - 1]), int(m[i + 1]), m[i])
+        result = calc(float(m[i - 1]), float(m[i + 1]), m[i])
         m[i-1]=' '
         m[i]=str(result)
         m[i+1]=' '         
-     m = list(filter(lambda a: a != ' ', m))     
-     return m
+        m = list(filter(lambda a: a != ' ', m))     
+        return m
+def calc_replace_priorities(m : []): # замены всех приоритетный операций их резултатами
+    while '*' in m or '/' in m:
+       m = calc_priority(m)
+    return m
 
-def calc_line_no_brackets(expression : str()):
-    array = expression.split()
-    print(array)
-    array_copy = array.copy()
-    mm = calc_priorities(array)
-    print(mm)
+def convert(n : str()): # формирует нужный массив из строки
+    lst = []
+    temp =''
+    for item in n:      
+      if item.isdigit():
+         temp += item
+      elif item =='*' or item == '/' or item == '+' or item == '-':
+        if len(temp) > 0:
+           lst.append(temp)
+           temp = ''
+           lst.append(item)
+    if len(temp) > 0:
+      lst.append(temp)
+      temp = ''
+    return lst
+
+def calc_line_no_brackets(expression : str()): # вычисление простой строки, без скобок
+    lst = []
+    lst = convert(expression)  
+    array_copy = lst.copy()     
+    mm = calc_replace_priorities(lst)    
     for field in array_copy:
       print(field, end = ' ')
     result = calc_no_priorities(mm)
     print('=', result) 
     return result
 
-n = '22 * 300 - 14 + 10 * 10'
+# примеры ввода
+#n = '22 * 300 - 14 + 10 * 10' # аккуратный пользователь
+#n = '  22* ,300-14 ;+  10* 10 _' # дрож в руках
+#n = '10 + 40 / 5 - 5' # все хорошо
+n = ' 10 ;*  5/2 ,+ 10/ 2 - 15_' # что-то не то ...
+
 result = calc_line_no_brackets(n)
-print(result)
+print('Результат: ',int(result))
 
 
 
