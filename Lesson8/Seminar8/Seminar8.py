7e9502828ce746d564b9fb95ad8326f8f8b753f7
agontar <agontar@yandex.ru>
2023-01-24 12:51:59 +0300

    add priorities functions

--- Lesson8/Seminar8/Seminar8.py
+++ Lesson8/Seminar8/Seminar8.py
@@ -1,11 +1,3 @@
-n = '22 * 300 - 14 + 10 * 10'
-
-m = n.split()
-m2 = []
-m2 = m.copy()
-print(m)      
-
-
 def calc(a, b, ch):
     if ch == '+':
         return a + b
@@ -16,43 +8,38 @@ def calc(a, b, ch):
     elif ch == '*':
         return a * b
 
+def calc_no_priorities(m : []):
+     result = int(m[0])
+     for i in range(1, len(m) - 1, 2):
+         result = calc(result, int(m[i + 1]), m[i])         
+     return result
+ 
+def calc_priorities(m : []):
+     for i in range(1, len(m) - 1, 2):
+       if m[i] == '*' or m[i] == '/':
+        result = calc(int(m[i - 1]), int(m[i + 1]), m[i])
+        m[i-1]=' '
+        m[i]=str(result)
+        m[i+1]=' '         
+     m = list(filter(lambda a: a != ' ', m))     
+     return m
 
-# a = int(m[0])
-# c = m[1]
-# b = int(m[2])
+def calc_line_no_brackets(expression : str()):
+    array = expression.split()
+    print(array)
+    array_copy = array.copy()
+    mm = calc_priorities(array)
+    print(mm)
+    for field in array_copy:
+      print(field, end = ' ')
+    result = calc_no_priorities(mm)
+    print('=', result) 
+    return result
 
-result = int(m[0])
-for i in range(1, len(m) - 1, 2):
-         result = calc(result, int(m[i + 1]), m[i])
-         print(result)
+n = '22 * 300 - 14 + 10 * 10'
+result = calc_line_no_brackets(n)
+print(result)
 
-#for i in range(1, len(m) - 1, 2):
-#   if m[i] == '*' or m[i] == '/':
-#        result = calc(int(m[i - 1]), int(m[i + 1]), m[i])
-#       m2.append(result)
-#    else:
-#        m2.append(m[i])
-#        m2.append(int(m[i + 1]))
-for i in range(1, len(m2) - 1, 2):
-   if m2[i] == '*' or m2[i] == '/':
-        result = calc(int(m2[i - 1]), int(m2[i + 1]), m2[i])
-        m2[i-1]=' '
-        m2[i]=str(result)
-        m2[i+1]=' '
-#new_str = ''
-#for item in m2:
-#   new_str += item
-
-#print(m[i], m[i + 1])
-print(m2)
-#print(new_str)
-m2 = list(filter(lambda a: a != ' ', m2))
-print(m2)
-result = int(m2[0])
-for i in range(1, len(m2) - 1, 2):
-         result = calc(result, int(m2[i + 1]), m2[i])
-         print(result)
-#print(result)
 
 
 
