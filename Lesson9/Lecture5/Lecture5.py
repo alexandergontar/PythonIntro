5e2b25d4c04f9642683fc360ead2b1f389bfcb85
agontar <agontar@yandex.ru>
2023-01-28 10:20:24 +0300

    add simple bot with RPN function

--- Lesson9/Lecture5/Lecture5.py
+++ Lesson9/Lecture5/Lecture5.py
@@ -1,37 +1,21 @@
-#import telebot
-import wikipedia
-import re
-import os
-import spy
 import bot_commands
-#import time
 import telebot
-#import telegram
-#import bot_commands as bc
-#from telegram import Update
-#from telegram.ext import Updater,  CommandHandler, CallbackContext
-#from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-#from bot_commands import *
-#from telegram import Update
-#from telegram.ext import Updater, CommandHandler, CallbackContext
-#from bot_commands import *
-TOKEN = '5923725347:AAHJXDY76Dghdu5bU_doA9C0awRhy0D4zDk'
-#updater = Updater(TOKEN)
-#app = ApplicationBuilder().token('5923725347:AAHJXDY76Dghdu5bU_doA9C0awRhy0D4zDk').build()
-bot = telebot.TeleBot(TOKEN)
+import conf
 
+bot = telebot.TeleBot(conf.TOKEN)
 
 @bot.message_handler(commands=[ 'help'])
 def send_welcome(message):
-    bot.reply_to(message, 'Добро пожаловать, напишите через пробел 2 цифры: ')
+    bot.reply_to(message, 'Добро пожаловать, введите арифметическое выражение, например 1 + 2 * (7 - 1) + (4 - 2) / 2 : ')
 
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Введите через пробел 2 цифры: ')
+    bot.send_message(m.chat.id, 'Введите арифметическое выражение: ')
+
 @bot.message_handler(content_types=["text"])
-def run_commands(message): # Название функции не играет никакой роли    
-    bot.add_message_handler(bot_commands.sum_a_b(message))
+def run_commands(message):    
+    bot.add_message_handler(bot_commands.calc_exp(message))
 
 bot.infinity_polling()
 
-input()
+
--- Lesson9/Lecture5/bot_commands.py
+++ Lesson9/Lecture5/bot_commands.py
@@ -1,7 +1,9 @@
 import telebot
-TOKEN = '5923725347:AAHJXDY76Dghdu5bU_doA9C0awRhy0D4zDk'
-bot = telebot.TeleBot(TOKEN)
-def repeat_all_messages(message): # Название функции не играет никакой роли
+import RPN
+import conf
+
+bot = telebot.TeleBot(conf.TOKEN)
+def repeat_all_messages(message): 
     print(message.id, message.text)
     bot.send_message(message.chat.id, message.text)
 
@@ -18,3 +20,13 @@ def sum_a_b(message):
          print('неверный ввод')
          bot.send_message(message.chat.id, 'неверный ввод')
     return
+def calc_exp(message):
+    msg = message.text
+    try:
+         result = RPN.rpn_calc(msg)
+         answer = str(result)
+         bot.send_message(message.chat.id, f' {msg} = {answer}')
+    except:
+         print('неверный ввод')
+         bot.send_message(message.chat.id, 'неверный ввод')
+    
